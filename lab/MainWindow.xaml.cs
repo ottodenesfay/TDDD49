230603d175cda41beceb3efbba6d27a9e4efc088
Otto Denesfay <34476823+ottodenesfay@users.noreply.github.com>
2018-11-12 15:33:56 +0100

    Text, textbox and button added

--- lab/MainWindow.xaml.cs
+++ lab/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace lab
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello {firstNameText.Text}");
+        }
     }
 }
