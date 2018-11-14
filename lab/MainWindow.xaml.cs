1cf033d00b2737144505cfe4ac01baefe0bfcdf7
Otto Denesfay <34476823+ottodenesfay@users.noreply.github.com>
2018-11-14 16:38:59 +0100

    Connect and listen added

--- lab/MainWindow.xaml.cs
+++ lab/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +22,10 @@ namespace lab
     /// </summary>
     public partial class MainWindow : Window
     {
+        Socket connect_s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        Socket listen_s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +33,41 @@ namespace lab
 
         private void connectButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Select a username");
+            string ip_connect = IP.Text;
+            int port_connect = Convert.ToInt32(Port.Text);
+            IPEndPoint connect_ipe = new IPEndPoint(IPAddress.Parse(ip_connect), port_connect);
+
+            try
+            {
+                connect_s.Connect(connect_ipe);
+            }
+            catch (ArgumentNullException ae)
+            {
+                MessageBox.Show("ArgumentNullException : {0}" + ae.ToString());
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show("SocketException : {0}" + se.ToString());
+            }
+            catch (Exception ue)
+            {
+                MessageBox.Show("Unexpected exception : {0}" + ue.ToString());
+            }
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes("This is a test");
+            int bytesSent = connect_s.Send(msg);
+        }
+
+        private void listenButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ip_listen = IP.Text;
+            int port_listen = Convert.ToInt32(Port.Text);
+            IPEndPoint listen_ipe = new IPEndPoint(IPAddress.Parse(ip_listen), port_listen);
+            listen_s.Bind(listen_ipe);
+            listen_s.Listen(1);
+            MessageBox.Show("Listening: IP=" + ip_listen + " Port=" + port_listen);
+            byte[] bytes = new byte[1024];
+            int bytesRec = listen_s.Receive(bytes);
+            MessageBox.Show("Text received: " + System.Text.Encoding.ASCII.GetString(bytes, 0, bytesRec));
         }
     }
 }
